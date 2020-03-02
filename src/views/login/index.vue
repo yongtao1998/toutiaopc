034b6513b5a49a794312c4179166e871b80b933a
yongtao1998 <zhangyongtao1310@yeah.net>
2020-03-02 14:10:56 +0800

    登陆页面格式验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,19 +7,19 @@
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
       <!-- 表单 -->
-      <el-form style="margin-top:20px">
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form style="margin-top:20px" :model="loginForm" :rules="loginRules" ref="loginForm">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"  ></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input style="width:60%" placeholder="请输入验证码" v-model="loginForm.code"></el-input>
           <el-button plain style="float:right">获取验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="loginForm.checked">我已阅读同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%" type='primary'>登录</el-button>
+          <el-button style="width:100%" type='primary' @click="login">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -28,7 +28,47 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        checked: false
+      },
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^[1][3-9][0-9]{9}$/, message: '您输入的手机号格式不正确' }
+        ],
+        code: [
+          { required: true, message: '验证码不能为空' },
+          { pattern: /^\d{6}$/, message: '验证码应该是6位数字' }
+        ],
+        checked: [{
+          validator (rule, value, callback) {
+            value ? callback() : callback(new Error('您必须同意我们的条款和协议'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 1. 回调函数
+      // this.$refs.loginForm.validate((isOK) => {
+      //   if (isOK) {
+      //     console.log('校验通过')
+      //   } else {
+      //     console.log('校验不通过')
+      //   }
+      // })
+      // 2. promise
+      this.$refs.loginForm.validate().then(() => {
+        console.log('校验通过')
+      }).catch(() => console.log('校验失败')
+      )
+    }
+  }
 }
 </script>
 
