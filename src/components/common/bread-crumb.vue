f784c6a5d41da338353544f318e593b68e253d68
yongtao1998 <zhangyongtao1310@yeah.net>
2020-03-04 19:19:56 +0800

    封装el面包屑组件

--- src/components/common/bread-crumb.vue
+++ src/components/common/bread-crumb.vue
@@ -1,6 +1,12 @@
 <template>
-  <el-breadcrumb>
-      <el-breadcrumb-item>首页</el-breadcrumb-item>
+<!-- el 面包屑  -->
+  <el-breadcrumb separator-class="el-icon-arrow-right">
+    <!-- 相当于 router-link标签 -->
+      <el-breadcrumb-item :to="{ path:'/home' }">首页</el-breadcrumb-item>
+    <!-- 二级菜单是动态的 有两种传值方式 props传值 slot插槽传值 -->
+    <el-hreadcrumb-item>
+      <slot name="title"></slot>
+    </el-hreadcrumb-item>
   </el-breadcrumb>
 </template>
 
